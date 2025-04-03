0d8f6354bd69509cf356acb42f8b82338d0e814a
ElielJesusGarciaVela1 <eliel.vela@1pf.cz>
2025-04-03 13:41:58 +0200

    Delete Project directory

--- Project/src/BooleanTest.java
+++
@@ -1,9 +0,0 @@
-public class BooleanTest {
-    public static void main (String[] args) {
-        boolean hasHighIncome = false;
-        boolean hasGoodCredit = true;
-        boolean hasCriminalRecord = true;
-        boolean isEligible = (hasHighIncome || hasGoodCredit) && !hasCriminalRecord;
-        System.out.println(isEligible);
-    }
-}
